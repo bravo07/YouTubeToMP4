0db744fe327fd5d9a10afb933360b5af119eaab7
Oscar Evans <123draxpc123@gmail.com>
2017-12-25 19:02:28 +0000

    TidyUp

--- Tests/MainForm.cs
+++ Tests/MainForm.cs
@@ -15,9 +15,9 @@ using System.Windows.Forms;
 
 namespace Tests
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
